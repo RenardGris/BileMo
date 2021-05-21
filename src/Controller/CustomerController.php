d4798b598ed9473e1ba6db159e4276f431ee75f0
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-05-22 01:31:57 +0200

    Refacto : Extend FosResController

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,9 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Customer;
+use App\Exception\ResourceValidationException;
+use App\Service\Paginator;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Hateoas\UrlGenerator\UrlGeneratorInterface;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Put;
+use FOS\RestBundle\Controller\Annotations\Delete;
+use FOS\RestBundle\Controller\Annotations\View;
 
-class CustomerController extends AbstractController
+class CustomerController extends AbstractFOSRestController
 {
 
     public function index()
