0a6891811a6606746f5599a723a3c2bb995615e1
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-05-22 01:26:06 +0200

    Show User : Show details of the current logged user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
 
 class UserController extends AbstractController
 {
@@ -18,8 +22,23 @@ class UserController extends AbstractController
 
     }
 
-    public function show()
+    /**
+     * Show details for the current logged user
+     *
+     *
+     * @Get(
+     *     path = "/api/users",
+     *     name = "api_user_show"
+     * )
+     * @View(
+     *     statusCode = 200,
+     *     serializerGroups = {"global","details"}
+     * )
+     * @param EntityManagerInterface $manager
+     * @return string
+     */
+    public function show(EntityManagerInterface $manager)
     {
-
+        return $this->getUser();
     }
 }
