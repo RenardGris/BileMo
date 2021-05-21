413d9040c94406c0b657b10e101c2fbcc6ec7989
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-05-22 01:27:32 +0200

    Show Product : show details of the requested product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Service\Paginator;
+use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
 
 class ProductController extends AbstractController
 {
@@ -11,8 +20,20 @@ class ProductController extends AbstractController
 
     }
 
-    public function show()
+    /**
+     * @Get(
+     *     path = "/api/products/{id}",
+     *     name = "api_products_show",
+     * )
+     * @View(
+     *     statusCode = 200,
+     *     serializerGroups = {"details"}
+     * )
+     * @param Product $product
+     * @return Product
+     */
+    public function show(Product $product)
     {
-
+        return $product;
     }
 }
