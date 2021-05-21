51777bd504bb1d8c6b7ddc122b1a329afcd7ece6
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-05-22 01:34:56 +0200

    Index Customer : Return a paginated collection of customers for the current logged user

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -23,6 +23,38 @@ use FOS\RestBundle\Controller\Annotations\View;
 
 class CustomerController extends AbstractFOSRestController
 {
+    /**
+     * @Get(
+     *     path = "/api/customers",
+     *     name = "api_customers_index"
+     * )
+     * @View(
+     *     statusCode = 200,
+     *     serializerGroups = {"global"}
+     * )
+     * @param EntityManagerInterface $manager
+     * @param Request $request
+     * @param Paginator $paginator
+     * @param SerializerInterface $serializer
+     * @return Response
+     */
+    public function index(EntityManagerInterface $manager, Request $request, Paginator $paginator, SerializerInterface $serializer)
+    {
+        $data = $paginator->paginate(
+            $manager->getRepository(Customer::class),
+            $request->attributes->get('_route'),
+            $request->query->get('page', 1),
+            ["User" => $this->getUser()->getId()]
+        );
+
+        return new Response(
+            $serializer->serialize(
+                $data,
+                'json',
+                SerializationContext::create()
+                    ->setGroups(["Default", "items" => ["global", "user" => ["fromCustomer"]] ]))
+        );
+    }
 
     public function index()
     {
