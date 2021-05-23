67777a3555ad3b287e9e318b95197db8e84063b5
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-05-23 05:57:33 +0200

    Doc : delete requested customer

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -196,6 +196,18 @@ class CustomerController extends AbstractFOSRestController
      *     name = "api_customers_update",
      *     requirements = {"id"="\d+"}
      * )
+     *
+     * @OA\Put(
+     *     path="/api/customers/{id}",
+     *     tags={"Customers"},
+     *     security={{"BearerAuth"={}}},
+     *     @OA\Parameter(ref="#/components/parameters/id"),
+     *     @OA\RequestBody(ref="#/components/requestBodies/storeCustomer"),
+     *     @OA\Response(response=201, ref="#/components/responses/customerResource"),
+     *     @OA\Response(response=400, ref="#/components/responses/badRequest"),
+     *     @OA\Response(response=401, ref="#/components/responses/invalidToken"),
+     * )
+     *
      * @ParamConverter("newCustomer", converter="fos_rest.request_body")
      * @param Customer $customer
      * @param Customer $newCustomer
